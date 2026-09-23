b8ad80303107208ddca46bae20d885baec92322b
sunattha6688009 <sunattha.boo@student.mahidol.ac.th>
2026-09-23 23:45:58 +0700

    Update PWC_SudokuPuzzleTest.java

--- liu-qi_project1/Sudoku-master/src/test/java/sudoku/PWC_SudokuPuzzleTest.java
+++ liu-qi_project1/Sudoku-master/src/test/java/sudoku/PWC_SudokuPuzzleTest.java
@@ -14,7 +14,7 @@ public class PWC_SudokuPuzzleTest {
     // T1
     @Test
     public void testIsSlotAvailableBoundaryEmptyMutable() {
-        SudokuPuzzle puzzle = new SudokuPuzzleForAvailableTest(emptyBoard(), allMutable());
+        SudokuPuzzle puzzle = new SudokuPuzzleForTest(emptyBoard(), allMutable());
         Assert.assertTrue(puzzle.isSlotAvailable(0, 0));
     }
 
@@ -25,7 +25,7 @@ public class PWC_SudokuPuzzleTest {
         board[0][0] = "5";
         boolean[][] mutable = allMutable();
         mutable[0][0] = false;
-        SudokuPuzzle puzzle = new SudokuPuzzleForAvailableTest(board, mutable);
+        SudokuPuzzle puzzle = new SudokuPuzzleForTest(board, mutable);
         Assert.assertFalse(puzzle.isSlotAvailable(0, 0));
     }
 
@@ -34,7 +34,7 @@ public class PWC_SudokuPuzzleTest {
     public void testIsSlotAvailableInteriorEmptyImmutable() {
         boolean[][] mutable = allMutable();
         mutable[4][4] = false;
-        SudokuPuzzle puzzle = new SudokuPuzzleForAvailableTest(emptyBoard(), mutable);
+        SudokuPuzzle puzzle = new SudokuPuzzleForTest(emptyBoard(), mutable);
         Assert.assertFalse(puzzle.isSlotAvailable(4, 4));
     }
 
@@ -43,64 +43,62 @@ public class PWC_SudokuPuzzleTest {
     public void testIsSlotAvailableInteriorOccupiedMutable() {
         String[][] board = emptyBoard();
         board[4][4] = "5";
-        SudokuPuzzle puzzle = new SudokuPuzzleForAvailableTest(board, allMutable());
+        SudokuPuzzle puzzle = new SudokuPuzzleForTest(board, allMutable());
         Assert.assertFalse(puzzle.isSlotAvailable(4, 4));
     }
 
     // makeMove()
-    // Value (Valid/Invalid) x Position (In-range/Out-of-range) x Conflict (No/Yes) x Mutability (Mutable/Locked)
+    // Value (Valid/Invalid) x Position (Boundary/Interior) x Conflict (No/Yes) x Mutability (Mutable/Locked)
 
     // T1
     @Test
-    public void testMakeMoveValidInRangeNoConflictMutable() {
-        SudokuPuzzle puzzle = new SudokuPuzzleForMakeMoveTest(emptyBoard(), allMutable());
+    public void testMakeMoveValidBoundaryNoConflictMutable() {
+        SudokuPuzzle puzzle = new SudokuPuzzleForTest(emptyBoard(), allMutable());
         puzzle.makeMove(0, 0, "5", true);
         Assert.assertEquals("5", puzzle.getValue(0, 0));
     }
 
     // T2
     @Test
-    public void testMakeMoveValidInRangeConflictLocked() {
+    public void testMakeMoveValidBoundaryConflictLocked() {
         String[][] board = emptyBoard();
         board[0][4] = "5";
         boolean[][] mutable = allMutable();
         mutable[0][0] = false;
-        SudokuPuzzle puzzle = new SudokuPuzzleForMakeMoveTest(board, mutable);
+        SudokuPuzzle puzzle = new SudokuPuzzleForTest(board, mutable);
         puzzle.makeMove(0, 0, "5", true);
         Assert.assertEquals("", puzzle.getValue(0, 0));
     }
 
     // T3
     @Test
-    public void testMakeMoveValidOutOfRangeNoConflictLocked() {
+    public void testMakeMoveValidInteriorNoConflictLocked() {
         boolean[][] mutable = allMutable();
-        mutable[0][0] = false;
-        SudokuPuzzle puzzle = new SudokuPuzzleForMakeMoveTest(emptyBoard(), mutable);
-        puzzle.makeMove(-1, 0, "5", true);
-        Assert.assertEquals("", puzzle.getValue(0, 0));
+        mutable[4][4] = false;
+        SudokuPuzzle puzzle = new SudokuPuzzleForTest(emptyBoard(), mutable);
+        puzzle.makeMove(4, 4, "5", true);
+        Assert.assertEquals("", puzzle.getValue(4, 4));
     }
 
     // T4
     @Test
-    public void testMakeMoveInvalidInRangeNoConflictLocked() {
+    public void testMakeMoveInvalidBoundaryNoConflictLocked() {
         boolean[][] mutable = allMutable();
         mutable[0][0] = false;
-        SudokuPuzzle puzzle = new SudokuPuzzleForMakeMoveTest(emptyBoard(), mutable);
+        SudokuPuzzle puzzle = new SudokuPuzzleForTest(emptyBoard(), mutable);
         puzzle.makeMove(0, 0, "X", true);
         Assert.assertEquals("", puzzle.getValue(0, 0));
     }
 
     // T5
     @Test
-    public void testMakeMoveInvalidOutOfRangeConflictMutable() {
+    public void testMakeMoveInvalidInteriorConflictMutable() {
         String[][] board = emptyBoard();
-        board[0][4] = "5";
-        SudokuPuzzle puzzle = new SudokuPuzzleForMakeMoveTest(board, allMutable());
-        puzzle.makeMove(-1, -1, "X", true);
-        Assert.assertEquals("", puzzle.getValue(0, 0));
+        board[4][7] = "X";
+        SudokuPuzzle puzzle = new SudokuPuzzleForTest(board, allMutable());
+        puzzle.makeMove(4, 4, "X", true);
+        Assert.assertEquals("", puzzle.getValue(4, 4));
     }
-
-
 
     private String[][] emptyBoard() {
         String[][] board = new String[9][9];
@@ -122,16 +120,8 @@ public class PWC_SudokuPuzzleTest {
         return mutable;
     }
 
-    private class SudokuPuzzleForAvailableTest extends SudokuPuzzle {
-        public SudokuPuzzleForAvailableTest(String[][] board, boolean[][] mutable) {
-            super(9, 9, 3, 3, new String[] {"1","2","3","4","5","6","7","8","9"});
-            this.board = board;
-            this.mutable = mutable;
-        }
-    }
-
-    private class SudokuPuzzleForMakeMoveTest extends SudokuPuzzle {
-        public SudokuPuzzleForMakeMoveTest(String[][] board, boolean[][] mutable) {
+    private class SudokuPuzzleForTest extends SudokuPuzzle {
+        public SudokuPuzzleForTest(String[][] board, boolean[][] mutable) {
             super(9, 9, 3, 3, new String[] {"1","2","3","4","5","6","7","8","9"});
             this.board = board;
             this.mutable = mutable;
